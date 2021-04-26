ef4c6a90be1004f9091b2e820690d5f6cf66f020
forfeen <f.fin07@gmail.com>
2021-04-26 12:58:22 +0700

    connect with mongo

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useCreateIndex: true
+});
+
+mongoose.connection.on('connected', () => {
+    console.log('Connected to mongo');
+})
+
+mongoose.connection.on('error', (err) => {
+    console.log('Error!', err);
+})
 
 app.get('/', (req, res) => {
     res.send('Test');
