bfa97620c4963a80820db9f067ac76b627624fcd
forfeen <f.fin07@gmail.com>
2021-04-26 16:00:43 +0700

    add vaccine route

--- src/routes/vaccineRoutes.js
+++ src/routes/vaccineRoutes.js
@@ -10,4 +10,21 @@ router.get('/vaccines', async(req, res) => {
     res.send(vaccines);
 });
 
+router.post('/vaccines', async(req, res) => {
+    const {name, description, developer, average_per_dose, performance, link_info  } = req.body;
+
+    if (!name || !description || !developer || !average_per_dose || !performance || !link_info) {
+        return res
+            .status(422)
+            .send({error: 'Please input the data'}); 
+    }
+    try {
+        const vaccine = new Vaccine({name, description, developer, average_per_dose, performance, link_info});
+        await vaccine.save();
+        res.send(vaccine);
+    } catch(err) {
+        res.status(422).send({ error: err.message});
+    }
+
+});
 module.exports = router;
