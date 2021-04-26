73162accb3dd1b2a84ade36301b2706e94ca7f04
forfeen <f.fin07@gmail.com>
2021-04-26 16:02:16 +0700

    add user auth

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
 const User = mongoose.model('User');
 
 const router = express.Router();
 
 router.post('/signup', async(req, res) => {
     const {email, password} = req.body;
-
     try{
         const user = new User({email, password});
+        
         await user.save();
-        res.send('You made a post request');
+        const token = jwt.sign({userId: user._id}, 'My_SECRET_KEY');
+        res.send({token});
 
     } catch (err) {
         return res.status(422).send(err.message);
