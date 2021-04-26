48c0d9998dbff27e441f5f89fbd12c3f73b1364e
forfeen <f.fin07@gmail.com>
2021-04-26 13:25:41 +0700

    create vaacine routes

--- src/routes/vaccineRoutes.js
+++ src/routes/vaccineRoutes.js
@@ -1,2 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
+
+const Vaccine = mongoose.model('Vaccine');
+
+const router = express.Router();
+
+router.get('/vaccines', async(req, res) => {
+    const vaccines = await Vaccine.find({ vaacineId: req.vaccine})
+    res.send(vaccines);
+});
+
+module.exports = router;
